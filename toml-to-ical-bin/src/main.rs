908b6b2f4e63a663d651be2a656a80a45434e16a
David Wood <david@davidtw.co>
2024-01-04 15:31:14 +0000

    early implementation
    Signed-off-by: David Wood <david@davidtw.co>

--- toml-to-ical-bin/src/main.rs
+++ toml-to-ical-bin/src/main.rs
@@ -1,3 +1,114 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::{
+    ffi::OsStr,
+    fs::{read_to_string, File, OpenOptions},
+    io::{self, Error as IoError, Write},
+    path::{Path, PathBuf},
+};
+use thiserror::Error;
+use toml::{de::Error as TomlError, from_str};
+use toml_to_ical::{Calendar, External};
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Failed to open input calendar description: {0}")]
+    OpenInput(#[from] IoError),
+    #[error("Failed to deserialize input calendar description: {0}")]
+    ParseInput(#[from] TomlError),
+    #[error("Failed to open output: {0}")]
+    OpenOutput(IoError),
+    #[error("Failed to write to output: {0}")]
+    WriteOutput(IoError),
+}
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Opt {
+    // Path to the input calendar description.
+    input: PathBuf,
+    /// Specify path to write the iCalendar to [default: -]
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+struct Session;
+
+impl External for Session {
+    type Error = Error;
+
+    fn from_path(path: &Path) -> Result<Calendar, Error> {
+        let as_str = read_to_string(path)?;
+        from_str(&as_str).map_err(Into::into)
+    }
+}
+
+/// Returns `true` if the file type is a fifo.
+#[cfg(not(target_family = "unix"))]
+fn is_fifo(_: std::fs::FileType) -> bool {
+    false
+}
+
+/// Returns `true` if the file type is a fifo.
+#[cfg(target_family = "unix")]
+fn is_fifo(file_type: std::fs::FileType) -> bool {
+    use std::os::unix::fs::FileTypeExt;
+    file_type.is_fifo()
+}
+
+/// Wrapper around output writer which handles differences between stdout, file and pipe outputs.
+pub(crate) enum Output {
+    Stdout(io::Stdout),
+    File(File),
+    Pipe(File),
+}
+
+impl Output {
+    /// Create a `Output` from the input path (or "-" for stdout).
+    pub(crate) fn new(path: &OsStr) -> io::Result<Self> {
+        if path == "-" {
+            return Ok(Output::Stdout(io::stdout()));
+        }
+
+        let file =
+            OpenOptions::new().read(true).write(true).create(true).truncate(true).open(path)?;
+        if is_fifo(file.metadata()?.file_type()) {
+            Ok(Output::File(file))
+        } else {
+            Ok(Output::Pipe(file))
+        }
+    }
+}
+
+impl io::Write for Output {
+    fn flush(&mut self) -> io::Result<()> {
+        match self {
+            Output::Stdout(stdout) => stdout.flush(),
+            Output::Pipe(pipe) => pipe.flush(),
+            Output::File(file) => file.flush(),
+        }
+    }
+
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self {
+            Output::Stdout(stdout) => stdout.write(buf),
+            Output::Pipe(pipe) => pipe.write(buf),
+            Output::File(file) => file.write(buf),
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let opts = Opt::parse();
+
+    let calendar = Calendar::load::<Session>(&opts.input)?;
+
+    let mut output = if let Some(output) = opts.output {
+        Output::new(output.as_os_str())
+    } else {
+        Output::new(OsStr::new("-"))
+    }
+    .map_err(Error::OpenOutput)?;
+    write!(output, "{calendar}").map_err(Error::WriteOutput)?;
+
+    Ok(())
 }
--- toml-to-ical/src/lib.rs
+++ toml-to-ical/src/lib.rs
@@ -1,14 +1,414 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use chrono::TimeZone;
+use rrule::{RRule, RRuleSet, Unvalidated};
+use serde::Deserialize;
+use std::{
+    fmt,
+    ops::Deref,
+    path::{Path, PathBuf},
+};
+
+// Current name of toml-to-ical.
+const NAME: &str = env!("CARGO_PKG_NAME");
+// Current version of toml-to-ical.
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+trait DateTimeExt {
+    /// Convert from a `UtcDateTime` to a `TzDateTime`.
+    fn to_tzdatetime(&self) -> TzDateTime;
+
+    /// Generates an iCalendar date-time string format with the prefix symbols.
+    /// e.g. `:19970714T173000Z` or `;TZID=America/New_York:19970714T133000`
+    ///
+    /// ref: <https://tools.ietf.org/html/rfc5545#section-3.3.5>
+    fn to_ical_format(&self) -> String;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl DateTimeExt for UtcDateTime {
+    fn to_tzdatetime(&self) -> TzDateTime {
+        rrule::Tz::UTC.from_utc_datetime(&self.naive_utc())
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn to_ical_format(&self) -> String {
+        self.format("%Y%m%dT%H%M%SZ").to_string()
+    }
+}
+
+/// `chrono::DateTime` fixed to UTC.
+type UtcDateTime = chrono::DateTime<chrono::Utc>;
+/// `chrono::DateTime` using `rrule::Tz`.
+type TzDateTime = chrono::DateTime<rrule::Tz>;
+
+pub trait External {
+    type Error;
+
+    fn from_path(path: &Path) -> Result<Calendar, Self::Error>;
+}
+
+/// A calendar.
+#[derive(Deserialize)]
+pub struct Calendar {
+    /// Name of the calendar.
+    name: String,
+    /// Description of the calendar.
+    description: String,
+
+    /// Meta configuration (e.g. include other calendar descriptions).
+    meta: Option<Meta>,
+    /// List of events.
+    events: Vec<Event>,
+}
+
+impl Calendar {
+    pub fn load<E: External>(path: &Path) -> Result<Self, <E as External>::Error> {
+        let mut root = E::from_path(path)?;
+        if let Some(meta) = &root.meta {
+            for include in &meta.includes {
+                let mut child = E::from_path(include)?;
+                root.events.extend(child.events.drain(..));
+            }
+        }
+        Ok(root)
+    }
+}
+
+impl fmt::Display for Calendar {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "BEGIN:VCALENDAR")?;
+        writeln!(f, "VERSION:2.0")?;
+        writeln!(f, "PRODID:-//{NAME}//toml-to-ical {VERSION}//EN")?;
+        writeln!(f, "CALSCALE:GREGORIAN")?;
+        writeln!(f, "X-WR-CALNAME:{}", self.name)?;
+        writeln!(f, "X-WR-CALDESC:{}", self.description)?;
+        for event in &self.events {
+            event.fmt(f)?;
+        }
+        writeln!(f, "END:VCALENDAR")
+    }
+}
+
+/// Meta configuration.
+/// e.g. include other calendar descriptions.
+#[derive(Deserialize)]
+struct Meta {
+    /// Other calendar TOML files that events should be included from.
+    #[serde(default)]
+    includes: Vec<PathBuf>,
+}
+
+/// Datetime that an event was created.
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct Uid(String);
+
+impl fmt::Display for Uid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "UID:{}", self.0)
+    }
+}
+
+/// Datetime that an event was created.
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct CreatedOn(UtcDateTime);
+
+impl fmt::Display for CreatedOn {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "CREATED:{}", self.0.to_ical_format())
+    }
+}
+
+/// Datetime that an event was last modified.
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct LastModified(UtcDateTime);
+
+impl fmt::Display for LastModified {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "LAST-MODIFIED:{}", self.0.to_ical_format())
+    }
+}
+
+/// Title or short description of an event.
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct Title(String);
+
+impl fmt::Display for Title {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "SUMMARY:{}", self.0)
+    }
+}
+
+/// Long description of an event.
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct Description(String);
+
+impl fmt::Display for Description {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "DESCRIPTION:{}", self.0)
+    }
+}
+
+/// Datetime that an event will start.
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct Start(UtcDateTime);
+
+impl fmt::Display for Start {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "DTSTART:{}", self.0.to_ical_format())
+    }
+}
+
+impl Deref for Start {
+    type Target = UtcDateTime;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// Datetime that an event will end.
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct End(UtcDateTime);
+
+impl fmt::Display for End {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "DTEND:{}", self.0.to_ical_format())
+    }
+}
+
+/// Location of an event.
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct Location(String);
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "LOCATION:{}", self.0)
+    }
+}
+
+/// Status of the event.
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Status {
+    /// Event is not confirmed to be happening yet.
+    Tentative,
+    /// Event is confirmed to be happening.
+    Confirmed,
+    /// Event has been cancelled.
+    Cancelled,
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "STATUS:{}",
+            match self {
+                Status::Tentative => "TENTATIVE",
+                Status::Confirmed => "CONFIRMED",
+                Status::Cancelled => "CANCELLED",
+            }
+        )
+    }
+}
+
+/// Is a subscriber as free or busy during the event?
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Transparency {
+    // Subscriber is considered busy during the event.
+    Opaque,
+    // Subscriber is not considered busy during the event.
+    Transparent,
+}
+
+impl fmt::Display for Transparency {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "TRANSP:{}",
+            match self {
+                Transparency::Opaque => "OPAQUE",
+                Transparency::Transparent => "TRANSPARENT",
+            }
+        )
+    }
+}
+
+/// Organizer of an event.
+#[derive(Deserialize)]
+struct Organizer {
+    /// Name of event organiser.
+    name: String,
+    /// Email address of event organiser.
+    email: String,
+}
+
+impl fmt::Display for Organizer {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "ORGANIZER:CN={};mailto:{}", self.name, self.email)
+    }
+}
+
+/// Unit of time that recurrence happens on.
+#[derive(Clone, Copy, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Frequency {
+    /// Every year.
+    Yearly,
+    /// Every month.
+    Monthly,
+    /// Every week.
+    Weekly,
+    /// Every daily.
+    Daily,
+    /// Every hour.
+    Hourly,
+    /// Every minute.
+    Minutely,
+    /// Every second.
+    Secondly,
+}
+
+impl Into<rrule::Frequency> for Frequency {
+    fn into(self) -> rrule::Frequency {
+        match self {
+            Frequency::Yearly => rrule::Frequency::Yearly,
+            Frequency::Monthly => rrule::Frequency::Monthly,
+            Frequency::Weekly => rrule::Frequency::Weekly,
+            Frequency::Daily => rrule::Frequency::Daily,
+            Frequency::Hourly => rrule::Frequency::Hourly,
+            Frequency::Minutely => rrule::Frequency::Minutely,
+            Frequency::Secondly => rrule::Frequency::Secondly,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Deserialize)]
+struct RecurrenceRule {
+    /// Unit of time that recurrence happens on.
+    /// e.g. daily, weekly, monthly.
+    frequency: Frequency,
+    /// How many `frequency` between each event?
+    /// e.g. an weekly recurrence with interval of two is every other week.
+    interval: Option<u16>,
+    /// Number of recurrences.
+    count: Option<u32>,
+    /// Date after which there will be no more recurrences.
+    until: Option<UtcDateTime>,
+}
+
+impl Into<RRule<Unvalidated>> for RecurrenceRule {
+    fn into(self) -> RRule<Unvalidated> {
+        let mut rule = RRule::new(self.frequency.into());
+        if let Some(interval) = self.interval {
+            rule = rule.interval(interval);
+        }
+        if let Some(count) = self.count {
+            rule = rule.count(count);
+        }
+        if let Some(until) = self.until {
+            rule = rule.until(until.to_tzdatetime());
+        }
+        rule
+    }
+}
+
+#[derive(Default, Deserialize)]
+#[serde(transparent)]
+struct Exceptions(Vec<UtcDateTime>);
+
+impl fmt::Display for Exceptions {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if !self.0.is_empty() {
+            let exception_strs: Vec<_> = self.0.iter().map(|d| d.to_ical_format()).collect();
+            writeln!(f, "EXDATE:{}", exception_strs.join(","))
+        } else {
+            Ok(())
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct Event {
+    /// Globally unique identifier for this event.
+    uid: Uid,
+    /// Datetime that this event was created.
+    created_on: Option<CreatedOn>,
+    /// Datetime this event was last modified.
+    last_modified: Option<LastModified>,
+    /// Title or short description of this event.
+    title: Title,
+    /// Long description of this event.
+    description: Option<Description>,
+    /// Datetime that this event will start. Always in UTC.
+    start: Start,
+    /// Datetime that this event will end. Always in UTC.
+    end: End,
+    /// Location of this event.
+    location: Option<Location>,
+    /// Status of this event.
+    status: Option<Status>,
+    /// Is a subscriber is considered busy during the event?
+    transparency: Option<Transparency>,
+    /// Who is responsible for this event?
+    organizer: Option<Organizer>,
+    /// Should this event repeat, and if so, how?
+    #[serde(default)]
+    recurrence: Vec<RecurrenceRule>,
+    /// List of dates which are exceptions to the recurrence rules.
+    #[serde(default)]
+    exceptions: Exceptions,
+}
+
+impl fmt::Display for Event {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "BEGIN:VEVENT")?;
+
+        self.uid.fmt(f)?;
+        if let Some(created_on) = &self.created_on {
+            created_on.fmt(f)?;
+        }
+        if let Some(last_modified) = &self.last_modified {
+            last_modified.fmt(f)?;
+        }
+        self.title.fmt(f)?;
+        if let Some(description) = &self.description {
+            description.fmt(f)?;
+        }
+        self.end.fmt(f)?;
+        if let Some(location) = &self.location {
+            location.fmt(f)?;
+        }
+        if let Some(status) = &self.status {
+            status.fmt(f)?;
+        }
+        if let Some(transparency) = &self.transparency {
+            transparency.fmt(f)?;
+        }
+        if let Some(organizer) = &self.organizer {
+            organizer.fmt(f)?;
+        }
+
+        let start = self.start.to_tzdatetime();
+        let mut ruleset = RRuleSet::new(start);
+        for rule in &self.recurrence {
+            match Into::<RRule<Unvalidated>>::into(*rule).validate(start) {
+                Ok(rule) => {
+                    ruleset = ruleset.rrule(rule);
+                }
+                Err(e) => panic!("invalid rule! {e}"),
+            }
+        }
+        writeln!(f, "{ruleset}")?;
+
+        self.exceptions.fmt(f)?;
+
+        writeln!(f, "END:VEVENT")
     }
 }
